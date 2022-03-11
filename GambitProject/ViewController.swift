167e903783fda2470a15b3d76d8b5033e07c0af7
mamaevann <mamaeva.naidag@mail.ru>
2022-03-11 08:55:09 +0300

    Initial Commit

---
+++ GambitProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GambitProject
+//
+//  Created by Наида Мамаева on 11.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
